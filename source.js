ea4410f1dd7b21f556d52c9b4afebf8ec8122fec
kaptainKekss <dr.boo.1s.me@gmail.com>
2017-12-09 21:54:09 +0200

    98 changes done

--- source.js
+++ source.js
@@ -1,1 +1,35 @@
 //include 'util.js'
+$(document).ready(function() {
+    
+        // /* This is basic - uses default settings */
+        
+        // $("a#single_image").fancybox();
+        
+        // /* Using custom settings */
+        
+        // $("a#inline").fancybox({
+        //     'hideOnContentClick': true
+        // });
+    
+        /* Apply fancybox to multiple items */
+        
+        $(".grouped_elements").fancybox({
+            'transitionIn'	:	'elastic',
+            'transitionOut'	:	'elastic',
+            'speedIn'		:	600, 
+            'speedOut'		:	200, 
+            'overlayShow'	:	false
+        });
+
+        $(".grouped_elements").fancybox({
+            'titlePosition'	:	'over',
+            'onComplete'	:	function() {
+                $("#fancybox-wrap").hover(function() {
+                    $("#fancybox-title").show();
+                }, function() {
+                    $("#fancybox-title").hide();
+                });
+            }
+        });
+        
+    });
